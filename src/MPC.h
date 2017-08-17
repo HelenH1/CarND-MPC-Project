d7e1221708d4544e6b7a44ee53191e0c7584e942
Helen Herring <exclr8@gmail.com>
2017-08-16 20:01:32 -0400

    Add files via upload

--- src/MPC.h
+++ src/MPC.h
@@ -4,17 +4,33 @@
 #include <vector>
 #include "Eigen-3.3/Eigen/Core"
 
+#define DT 0.1 // time step duration dt in s
+
+// This value assumes the model presented in the classroom is used.
+//
+// It was obtained by measuring the radius formed by running the vehicle in the
+// simulator around in a circle with a constant steering angle and velocity on a
+// flat terrain.
+//
+// Lf was tuned until the the radius formed by the simulating the model
+// presented in the classroom matched the previous radius.
+//
+// This is the length from front to CoG that has a similar radius.
+#define LF 2.67
+
 using namespace std;
 
 class MPC {
- public:
-  MPC();
-
-  virtual ~MPC();
-
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+public:
+    MPC();
+    
+    virtual ~MPC();
+    
+    // Solve the model given an initial state and polynomial coefficients.
+    // Return the first actuatotions.
+    vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+    vector<double> mpc_x;
+    vector<double> mpc_y;
 };
 
 #endif /* MPC_H */
